6931586a8258a7a0689cd7f484d9f41b9aac3141
Mayukh Majumdar <Mayukh Majumdar>
2016-09-15 10:37:57 -0500

    First commit

---
+++ Bs1.py
@@ -0,0 +1,22 @@
+import requests
+import io
+import os.path
+from bs4 import BeautifulSoup
+url="http://www.imdb.com/chart/top?ref_=nv_mv_250_6"
+r = requests.get(url)
+bs = BeautifulSoup(r.text)
+save_path="F:/"
+completeName=os.path.join(save_path,"output.txt")
+g_data = bs.find_all("tbody",{"class":"lister-list"})
+rank=0
+for movie in g_data:
+    for trmovie in movie.find_all("tr"):
+        rank=rank+1
+        title= trmovie.find("td","titleColumn").contents[1].text
+        rating= trmovie.find("td","ratingColumn").contents[1].text
+        print rank,title,rating
+        movies =[title,rating]
+        fp=io.open(completeName, "a",encoding="utf-8")
+        fp.write(title)
+        fp.close()
+        
